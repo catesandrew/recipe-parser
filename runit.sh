6bbbab2e01096624a03cbb11a03bb104e0526da5
Andrew Cates <catesandrew@gmail.com>
2014-01-27 22:50:12 -0800

    Fix for ends with for work surface

--- runit.sh
+++ runit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for RECIPE in {83..89}
+for RECIPE in {86..89}
 do
     URL=http://mako.dev/cooksillustrated/recipes/recipe-${RECIPE}.html
     RESULT=`curl -o /dev/null --silent --head --write-out '%{http_code}\n' ${URL}`
