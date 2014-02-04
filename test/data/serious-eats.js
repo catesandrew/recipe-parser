0b4c00b45e1852cd4eb32df7f8b946f862e44fbd
Andrew Cates <catesandrew@gmail.com>
2014-02-03 21:12:36 -0800

    fixed low-sodium

--- test/data/serious-eats.js
+++ test/data/serious-eats.js
@@ -37,22 +37,24 @@ module.exports = [
     }
   },
   {
-    "2 tablespoons canola or vegetable oil": [{
-      "description": "Or",
-      "isDivider": true,
-      "ingredients": [
-        {
-          "quantity": "2",
-          "measurement": "tablespoons",
-          "description": "canola"
-        },
-        {
-          "quantity": "2",
-          "measurement": "tablespoons",
-          "description": "vegetable oil"
-        }
-      ]
-    }]
+    "2 tablespoons canola or vegetable oil": [
+      {
+        "description": "Or",
+        "isDivider": true,
+        "ingredients": [
+          {
+            "quantity": "2",
+            "measurement": "tablespoons",
+            "description": "canola"
+          },
+          {
+            "quantity": "2",
+            "measurement": "tablespoons",
+            "description": "vegetable oil"
+          }
+        ]
+      }
+    ]
   },
   {
     "1 tablespoon freshly minced garlic, about 3 medium cloves": {
@@ -96,6 +98,141 @@ module.exports = [
       "quantity": "1",
       "measurement": "teaspoon",
       "description": "sesame oil"
+    }
+  },
+  {
+    "4 whole dried ancho chilies, seeds and stems removed": {
+      "quantity": "4",
+      "measurement": "whole",
+      "description": "dried ancho chilies",
+      "direction": "seeds and stems removed"
+    }
+  },
+  {
+    "4 whole dried pasilla chilies, seeds and stems removed": {
+      "quantity": "4",
+      "measurement": "whole",
+      "description": "dried pasilla chilies",
+      "direction": "seeds and stems removed"
+    }
+  },
+  {
+    "1 quart (32 ounces) homemade or store-bought low-sodium chicken stock": {
+      "quantity": "1",
+      "measurement": "quart",
+      "description": "low-sodium chicken stock",
+      "alt": "32 ounces"
+    }
+  },
+  {
+    "1/2 cup raisins": {
+      "quantity": "1/2",
+      "measurement": "cup",
+      "description": "raisins"
+    }
+  },
+  {
+    "1 cup frozen orange juice concentrate": {
+      "quantity": "1",
+      "measurement": "cup",
+      "description": "frozen orange juice concentrate"
+    }
+  },
+  {
+    "3 whole chipotle chilies canned in adobo": {
+      "quantity": "3",
+      "measurement": "whole",
+      "description": "chipotle chilies canned in adobo"
+    }
+  },
+  {
+    "2 tablespoons white vinegar": {
+      "quantity": "2",
+      "measurement": "tablespoons",
+      "description": "white vinegar"
+    }
+  },
+  {
+    "2 tablespoons Asian fish sauce": {
+      "quantity": "2",
+      "measurement": "tablespoons",
+      "description": "Asian fish sauce"
+    }
+  },
+  {
+    "3 pounds boneless pork shoulder, trimmed and cut into 2-inch chunks": {
+      "quantity": "3",
+      "measurement": "pounds",
+      "description": "boneless pork shoulder",
+      "direction": "trimmed and cut into 2-inch chunks"
+    }
+  },
+  {
+    "2 tablespoons vegetable oil": {
+      "quantity": "2",
+      "measurement": "tablespoons",
+      "description": "vegetable oil"
+    }
+  },
+  {
+    "2 medium onions, thinly sliced (about 2 cups)": {
+      "quantity": "2",
+      "measurement": "medium",
+      "description": "onions",
+      "direction": "thinly sliced",
+      "alt": "about 2 cups"
+    }
+  },
+  {
+    "6 medium cloves garlic, minced (about 2 tablespoons)": {
+      "quantity": "6",
+      "measurement": "medium",
+      "description": "cloves garlic",
+      "direction": "minced",
+      "alt": "about 2 tablespoons",
+      "finale": {
+        "quantity": "6",
+        "measurement": "medium",
+        "description": "garlic cloves",
+        "direction": "minced",
+        "alt": "about 2 tablespoons",
+      }
+    }
+  },
+  {
+    "2 teaspoons dried oregano": {
+      "quantity": "2",
+      "measurement": "teaspoons",
+      "description": "dried oregano"
+    }
+  },
+  {
+    "1 tablespoon ground cumin": {
+      "quantity": "1",
+      "measurement": "tablespoon",
+      "description": "cumin",
+      "direction": "ground"
+    }
+  },
+  {
+    "3 bay leaves": {
+      "quantity": "3",
+      "description": "bay leaves"
+    }
+  },
+  {
+    "Kosher salt": {
+      "description": "Kosher salt",
+      "finale": {
+        "description": "kosher salt",
+      }
+    }
+  },
+  {
+    "Corn tortillas, cilantro, diced onions, lime wedges, and queso fresco for serving (optional)": {
+      "description": "Corn tortillas",
+      "direction": "cilantro, diced onions, lime wedges, and queso fresco for serving",
+      "alt": "optional"
     }
   }
 ];
